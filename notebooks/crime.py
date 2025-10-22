4a23f319aa0e23c431e1e3b5ee7f532050a2dc8b
masonralls <masonralls@mail.weber.edu>
2025-10-21 20:09:17 -0600

    Continuation of library installations and pip upgrade, activation of kernel.

--- notebooks/crime.py
+++ notebooks/crime.py
@@ -4,3 +4,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+
+# load the dataset
+path = "C:\\Users\\mason\\OneDrive\\Desktop\\MATH_3310\\project_1_crime\\MATH3310_FA_2025_01_01_RALLS\\data\\raw\\crime.csv"
+df = pd.read_csv(path)
+df.head()
+
+# Display basic information about the dataset
+print(df.info())
+
+# Check for missing values
+print(df.isnull().sum())
